f214908d27802de6e9982322c2803cf8f3b41095
Kyra-Burns <kyburns@g.hmc.edu>
2025-03-25 23:08:57 -0700

    Update XYStateEstimator.cpp
    4.3 Updates

--- libraries/main/XYStateEstimator.cpp
+++ libraries/main/XYStateEstimator.cpp
@@ -23,6 +23,23 @@ void XYStateEstimator::updateState(imu_state_t * imu_state_p, gps_state_t * gps_
   if (gps_state_p->num_sat >= N_SATS_THRESHOLD){
     gpsAcquired = 1;
 
+    // Convert latitude and longitude length to m 
+    float dLat = gps_state_p -> lat - orgin_lat;   //Degrees 
+    float dLon = gps_state_p -> lon - orgin_lon;
+    //Radians 
+    float dLatRad = dLat * (PT/180);
+    float dLonRad = dLon * (PT/180);
+    //Equirectangular Projection 
+    state.x = RADIUS_OF_EARTH_M*cos(orgin_lat * (PI/180)) * dLonRad; //East 
+    state.y = RADIUS_OF_EARTH_M * dLatRad;
+
+    // Convert IMY heading - CW from North to yaw - CCW from East 
+    float headingRad = imu_state_p -> heasing * (PI/180);
+    float offset = PI/2; // Rotating 90 deg from north to east 
+    state.yaw = angleDiff(offset - headingRad) // Flip sign for CW to CCw 
+    
+    
+
     // set the values of state.x, state.y, and state.yaw
     // It can make use of the constants RADIUS_OF_EARTH, origin_lat, origin_lon (see XYStateEstimator.h)
     // You can access the current GPS latitude and longitude readings with gps_state_p->lat and gps_state_p->lon
