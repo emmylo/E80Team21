0753ae8ca00f3d010d18acdeb89ab1a782d1e02b
Emmy Lo <108785218+emmylo@users.noreply.github.com>
2025-04-07 15:28:09 -0700

    potentiometer code

---
+++ libraries/main/PotentiometerVane.cpp
@@ -0,0 +1,31 @@
+#include "Pinouts.h"
+#include "PotentiometerVane.h"
+
+
+
+int main() {
+
+    // get R1 into range
+    while (R1 >= 6000 || R1 <= 4000) {
+        if (R1 > 6000) {
+            R1 -= 1000;  // Decrease R1 by 1000 if it's greater than 6000
+        } else {
+            R1 += 1000;  // Increase R1 by 1000 if it's less than 4000
+        }
+    }
+
+    if(R1>=5000){
+        R1 -= 5000;
+        angle = R1/1000;
+
+    }
+    else{
+        R1 -= 4000;
+        angle = R1/1000;
+    }
+    
+    angle = 360 - angle*360; // degrees
+
+
+}
+
