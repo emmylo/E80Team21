ee83719aeab8546d347b3645775f993c02eb82d7
Emmy Lo <108785218+emmylo@users.noreply.github.com>
2025-04-23 14:46:34 -0700

    Update MainNavigation.h

--- libraries/main/MainNavigation.h
+++ libraries/main/MainNavigation.h
@@ -55,8 +55,8 @@ public:
   float Vdivider; //back out to get Vout from voltage divider
   float R1; // back out to get resistance 
 
-  bool navigateState = 1; // did i change this?
-  bool atPoint = 0;
+  bool navigateState = 1; 
+  bool atPoint;
   bool complete = 0;
   bool navMode = 0; // 0 for GPS, 1 for vane
 
